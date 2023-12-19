a8d7cece905569db9e4717d974268849899727bb
jealmeida021 <jealmeida021@gmail.com>
2023-12-19 17:07:46 -0300

    Exercicio Biblioteca

---
+++ src/Livro.java
@@ -0,0 +1,20 @@
+public class Livro {
+
+    private String titulo;
+    private String autor;
+    private double pontuacao;
+
+
+    public Livro (String titulo, String autor, double pontuacao ){
+    this.titulo = titulo;
+    this.autor = autor;
+    this.pontuacao = pontuacao;
+
+    }
+
+    public String getTitulo(){
+    return titulo;
+    }
+
+
+}
